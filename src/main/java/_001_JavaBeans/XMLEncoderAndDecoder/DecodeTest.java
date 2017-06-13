d5b73d87ac02411335042e9bb96fa00621c3d690
mdolata <mateuszdolata2@gmail.com>
2017-06-13 21:31:13 +0200

    Add XMLDecoder test class

---
+++ src/main/java/_001_JavaBeans/XMLEncoderAndDecoder/DecodeTest.java
@@ -0,0 +1,28 @@
+package _001_JavaBeans.XMLEncoderAndDecoder;
+
+
+import java.beans.XMLDecoder;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * Created by mdolat on 13.06.2017 , 21:20.
+ *
+ */
+public class DecodeTest {
+    public static void main(String[] args){
+        try {
+            XMLDecoder xmlDecoder = new XMLDecoder(
+                    new BufferedInputStream(
+                            new FileInputStream("Test.xml")
+                    )
+            );
+
+            Example example = (Example) xmlDecoder.readObject();
+            System.out.println(example);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
